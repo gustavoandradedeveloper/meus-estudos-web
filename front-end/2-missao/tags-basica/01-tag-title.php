c09f4c1c5c982e7fd0f24bba71d58a47f56ea1d2
Gustavo Andrade <66392979+gustavoandradedeveloper@users.noreply.github.com>
2022-01-05 19:33:39 -0300

    TAG BÁSICA - TAG TITLE
    ESTUDANDO A TAG TITLE EXEMPLO PRÁTICO !

---
+++ front-end/2-missao/tags-basica/01-tag-title.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+	    <title>Tags básica do HTML - Tag Title</title>
+    </head>
+
+    <body>
+
+    	<h1>Tag Title</h1>
+        <p>A tag title é responsável por definir o título de uma página web.</p>
+        
+        <p>
+        	O conteúdo do título de uma página é muito importante para a otimização do mecanismo de pesquisa (SEO)! O título da página é usado pelos algoritmos do mecanismo de pesquisa para decidir a ordem ao listar as páginas nos resultados da pesquisa.
+        </p>	
+    </body>     
+
+</html>
