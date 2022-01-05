dc130d8705f8394f88a4a8b6ae37f34b1106cdee
Gustavo Andrade <66392979+gustavoandradedeveloper@users.noreply.github.com>
2022-01-05 19:59:14 -0300

    Tag para marcar um paragrafo no documento html
    Tag para marcar um paragrafo no html!

---
+++ front-end/2-missao/tags-basica/03-tag-paragrafo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>	Tag de Parágrafo</title>
+</head>
+<body>
+
+	<p>Título de nível 1</p>
+
+	<p>
+		O<a href="#">
+				Devmedia
+		</a>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	</p>
+
+
+</body>
+</html>
