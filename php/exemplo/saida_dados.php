916fddb2dc26bd7f9109d6c946af8cd70f3fb56e
Gustavo Andrade <66392979+gustavoandradedeveloper@users.noreply.github.com>
2022-01-07 23:19:38 -0300

    comando de saída em PHP
    comando de saída em PHP!!

---
+++ php/exemplo/saida_dados.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<?php 
+
+
+	echo'Olá mundo';
+	print_r('me livrei da maldição');
+
+
+	?>
+</body>
+</html>
