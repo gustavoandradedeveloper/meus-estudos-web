bce69ce2bb8a1b4c7771d1db45d408e3d09694a0
Gustavo Andrade <66392979+gustavoandradedeveloper@users.noreply.github.com>
2022-02-15 23:11:59 -0300

    Forma correta de declarar constante no PHP
    Forma correta de declarar constante no PHP!

--- php/exemplo/06-constante.php
+++ php/exemplo/06-constante.php
@@ -1,8 +1,16 @@
 <?php
 
+    //forma de declaração errada
     $BD_URL = '';
     $BD_USUARIO = "root";
     $BD_SENHA = "";
 
     echo $BD_USUARIO ;
+
+
+    define('BD_URL','');
+    define('BD_USUARIO','root');
+    define('BD_SENHA','');
+
+    echo 
 ?>
