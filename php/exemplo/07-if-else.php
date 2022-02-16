7da7a34ecca1bdffb9f3164d0f1774c008871ad8
Gustavo Andrade <66392979+gustavoandradedeveloper@users.noreply.github.com>
2022-02-16 00:02:01 -0300

    Operadores de comparação
    Estudando operadores de comparação em PHP!

---
+++ php/exemplo/07-if-else.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        // IGUAL ( == )
+        // if(5 == 5){
+        //     echo "verdadeiro";
+        // }else{
+        //     echo 'falso';
+        // }
+        
+        
+        // IDÊNTICO ( === ) tipo precisar ser igual
+        // $a = 5 ;
+        // $b = "5"; 
+        // if($a === $b){
+        //     echo "verdadeiro";
+        // }else{
+        //     echo 'falso';
+        // }
+
+        // IDÊNTICO ( === ) tipo precisar ser igual
+        // $a = 5 ;
+        // $b = "5"; 
+        // if($a !== $b){
+        //     echo "verdadeiro";
+        // }else{
+        //     echo 'falso';
+        // }
+
+
+        // // Diferente ( != )
+        // if(5 != 4){
+        //     echo "verdadeiro";
+        // }else{
+        //     echo 'falso';
+        // }
+
+        //  // Diferente ( <> )
+        //  if(5 <> 4){
+        //     echo "verdadeiro";
+        // }else{
+        //     echo 'falso';
+        // }
+
+        //  Menor ( < )
+        //  if(5 < 8){
+        //     echo "verdadeiro";
+        // }else{
+        //     echo 'falso';
+        // }
+
+        
+        // //  // Maior ( > )
+        // if(5 > 8){
+        //     echo "verdadeiro";
+        // }else{
+        //     echo 'falso';
+        // }
+
+        // //  // Menor ou igual ( > )
+        // if(8 <= 8){
+        //     echo "verdadeiro";
+        // }else{
+        //     echo 'falso';
+        // }
+
+         // Maior igual ( > )
+        if( 9 >= 8){
+            echo "verdadeiro";
+        }else{
+            echo 'falso';
+        }
+    ?>
+</body>
+</html>
