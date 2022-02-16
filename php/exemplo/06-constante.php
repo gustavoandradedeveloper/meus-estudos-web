0a775ff1f4fbb608501f5bab3dc1366e78085633
Gustavo Andrade <66392979+gustavoandradedeveloper@users.noreply.github.com>
2022-02-15 23:43:52 -0300

    removendo código não utilizado!
    removendo código não utilizado!

--- php/exemplo/06-constante.php
+++ php/exemplo/06-constante.php
@@ -1,16 +1,15 @@
 <?php
 
     //forma de declaração errada
-    $BD_URL = '';
-    $BD_USUARIO = "root";
-    $BD_SENHA = "";
+    // $BD_URL = '';
+    // $BD_USUARIO = "root";
+    // $BD_SENHA = "";
 
-    echo $BD_USUARIO ;
+    // echo $BD_USUARIO ;
 
 
     define('BD_URL','');
     define('BD_USUARIO','root');
     define('BD_SENHA','');
 
-    echo 
 ?>
